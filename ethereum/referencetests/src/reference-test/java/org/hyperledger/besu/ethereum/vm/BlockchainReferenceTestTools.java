5fa195008258ea2349de68c4c7f5679873a91365
Danno Ferrin <danno.ferrin@swirldslabs.com>
2022-11-06 00:03:04 -0600

    ref tests for Shandong
    Signed-off-by: Danno Ferrin <danno.ferrin@swirldslabs.com>

--- ethereum/referencetests/src/reference-test/java/org/hyperledger/besu/ethereum/vm/BlockchainReferenceTestTools.java
+++ ethereum/referencetests/src/reference-test/java/org/hyperledger/besu/ethereum/vm/BlockchainReferenceTestTools.java
@@ -49,7 +49,7 @@ public class BlockchainReferenceTestTools {
             "test.ethereum.blockchain.eips",
             "FrontierToHomesteadAt5,HomesteadToEIP150At5,HomesteadToDaoAt5,EIP158ToByzantiumAt5,"
                 + "Frontier,Homestead,EIP150,EIP158,Byzantium,Constantinople,ConstantinopleFix,Istanbul,Berlin,"
-                + "London,Shanghai,Cancun");
+                + "London,Shandong,Shanghai,Cancun");
     NETWORKS_TO_RUN = Arrays.asList(networks.split(","));
   }
 
